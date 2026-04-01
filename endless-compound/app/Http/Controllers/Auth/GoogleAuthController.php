aeadc662960741d0dd10b0ca86f366c9284e6400
ALEK20GG <alez.lego@gmail.com>
2026-04-01 15:07:33 +0200

    ganja

---
+++ endless-compound/app/Http/Controllers/Auth/GoogleAuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleAuthController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $googleUser = Socialite::driver('google')->user();
+
+            $user = User::where('email', $googleUser->getEmail())->first();
+
+            if ($user) {
+                // User exists, log them in
+                Auth::login($user);
+            } else {
+                // Create new user
+                $user = User::create([
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                    'password' => Hash::make(uniqid()), // Random password since they login via Google
+                    'email_verified_at' => now(), // Google accounts are verified
+                ]);
+
+                Auth::login($user);
+            }
+
+            return redirect()->intended('/dashboard');
+        } catch (\Exception $e) {
+            return redirect('/login')->withErrors(['google' => 'Unable to login with Google.']);
+        }
+    }
+}
--- setup.md
+++ setup.md
@@ -1,4 +1,3 @@
-composer create-project laravel/laravel endless-compound
 composer require laravel/breeze --dev
 php artisan breeze:install
 npm install
