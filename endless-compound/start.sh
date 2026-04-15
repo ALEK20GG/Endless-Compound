ff8d3ffec959cfacac67da576c415efe5c50f1bd
ALEK20GG <alez.lego@gmail.com>
2026-04-15 09:19:46 +0200

    another db fix test

--- endless-compound/start.sh
+++ endless-compound/start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -e
+
+# Genera APP_KEY se mancante
+if [ -z "$APP_KEY" ]; then
+    echo "APP_KEY mancante, la genero..."
+    php artisan key:generate --force
+fi
+
 php artisan config:cache
 php artisan route:cache
+php artisan view:cache
 php artisan migrate --force
 apache2-foreground
